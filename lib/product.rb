f4b88d90608b2c3e45c4e5e5472f20d4e01c5f05
dmarsenault <dmarsenault@gmail.com>
2016-02-06 22:16:53 -0700

    Add find by title option

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,27 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
     add_to_products
+    @price = options[:price]
+    @stock = options[:stock]
   end
 
   def self.all
     @@products
   end
+
+  def self.find_by_title(product_name)
+    @@products.each do |product|
+      if product.title == product_name
+        return product
+      end
+    end
+  end
+
 
   private
 
