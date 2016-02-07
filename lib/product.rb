db1a0aa18062baf29b945eb0ed31d8c02bb2a579
dmarsenault <dmarsenault@gmail.com>
2016-02-06 22:03:39 -0700

    Fixed Error.

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  private
+
+  def add_to_products
+    @@products.each do |product|
+      if product.title == self.title
+        raise DuplicateProductError, "'#{self.title}' already exists."
+      end
+    end
+        @@products << self
+  end
+
 end
