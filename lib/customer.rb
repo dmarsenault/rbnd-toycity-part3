3799ffdb73fc08cdfb258108b6cd811963eb1728
dmarsenault <dmarsenault@gmail.com>
2016-02-06 22:42:55 -0700

    Add customer section

---
+++ lib/customer.rb
@@ -0,0 +1,34 @@
+class Customer
+  attr_reader :name
+
+  @@customers = Array.new
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(customer_name)
+    @@customers.each do |customer|
+      if customer.name == customer_name
+        return customer
+      end
+    end
+  end
+
+  private
+
+  def add_to_customers
+    @@customers.each do |customer|
+      if customer.name == self.name
+        raise DuplicateCustomerError "'#{self.name}' already exists."
+      end
+    end
+  @@customers << self
+  end
+
+end
